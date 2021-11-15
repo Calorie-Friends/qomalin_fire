a14e81fa609ecfe4808f54c7a9cd1489da80e9ac
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-11-15 19:29:07 +0900

    [change] 型を定義しました。命名を変更しました

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,20 +9,67 @@ import * as maps from "@googlemaps/google-maps-services-js";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
 const GOOGLE_MAP_API_TOKEN: string = functions.config().google_map.token;
 admin.initializeApp();
 const firestore = admin.firestore();
 
-export const onCreateUser = functions.auth.user().onCreate((user) => {
-  const uid = user.uid;
+interface UserData {
+  username: string;
+  avatarIcon: string | undefined;
+}
+
+/**
+ * Userを表すオブジェクト
+ */
+class User {
+  id: string;
+  username: string;
+  avatarIcon: string | undefined;
+
+  /**
+   * 
+   * @param {string} id 
+   * @param {UserData} data Documentと対応するUserData
+   */
+  constructor(id: string, data: UserData) {
+    this.username = data.username;
+    this.id = id;
+    this.avatarIcon = data.avatarIcon;
+  }
+}
+
+
+const userConverter: FirebaseFirestore.FirestoreDataConverter<User> = {
+  fromFirestore(snapshot: FirebaseFirestore.QueryDocumentSnapshot) {
+    const data = snapshot.data();
+    const udata = data as UserData;
+    return new User(snapshot.id, udata);
+  },
+  toFirestore: (model: User) => {
+    return {
+      username: model.username,
+      avatarIcon: model.avatarIcon
+    };
+  }
+};
+
+
+export const onCreateUser = functions.auth.user().onCreate((authUser) => {
+  const uid = authUser.uid;
   firestore.runTransaction(async () => {
     const username = await firestore.collection("usernames").add({
       user_id: uid,
     });
-    const user = await firestore.collection("users").add({
-      username: username.id,
-      avatar_icon: null,
-    });
+    const user = await firestore.collection("users").withConverter(userConverter).add(
+      new User(
+        "", 
+        {
+          username: username.id,
+          avatarIcon: authUser.photoURL,
+        }
+      )
+    );
     return user;
   });
 
@@ -31,10 +78,10 @@ export const onDeleteUser = functions.auth.user().onDelete((user) => {
   const batch = firestore.batch();
   firestore.runTransaction(async ()=> {
     const questions = await firestore.collection("questions")
-      .where("user_id", "==", user.uid).get();
+      .where("userId", "==", user.uid).get();
     questions.docs.map((q) => batch.delete(q.ref));
     const answers = await firestore.collectionGroup("answers")
-      .where("user_id", "==", user.uid).get();
+      .where("userId", "==", user.uid).get();
     answers.docs.map((a) => batch.delete(a.ref));
     batch.delete(firestore.collection("users").doc(user.uid));
     batch.commit();
